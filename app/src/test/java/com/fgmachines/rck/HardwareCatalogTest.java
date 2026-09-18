149348f78040c2f6adf668eef113238bc9fe3f5f
NEXVARY Inc <325503741+nexvary@users.noreply.github.com>
2026-09-18 08:26:08 -0700

    Test Dawon MTD-01 hardware identification

---
+++ app/src/test/java/com/fgmachines/rck/HardwareCatalogTest.java
@@ -0,0 +1,41 @@
+package com.fgmachines.rck;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HardwareCatalogTest {
+    @Test public void identifiesDawonFromStickerModel() {
+        HardwareCatalog.Profile p = HardwareCatalog.identifyFromLabel("Model MTD-01");
+        assertNotNull(p);
+        assertEquals("dawon_mtd_01", p.id);
+        assertEquals(HardwareCatalog.Transport.Z_WAVE_GATEWAY, p.transport);
+        assertTrue(p.requiresGateway());
+    }
+
+    @Test public void identifiesDawonFromCertificationModel() {
+        HardwareCatalog.Profile p = HardwareCatalog.identifyFromLabel(
+                "MSIP-CMM-DaW-PM-M130-ZW JH04151-17006");
+        assertNotNull(p);
+        assertEquals("MTD-01 / PM-M130-ZW", p.displayModel);
+        assertEquals(2, p.usbPortCount);
+        assertEquals(3500, p.maxPowerW);
+    }
+
+    @Test public void doesNotTreatDawonAsMttl() {
+        HardwareCatalog.Profile p = HardwareCatalog.identifyFromLabel("PM-M130-ZW");
+        assertNotNull(p);
+        assertNotEquals(HardwareCatalog.Transport.MTTL_LOCAL_TCP, p.transport);
+    }
+
+    @Test public void keepsMttlFamilyRecognition() {
+        HardwareCatalog.Profile p = HardwareCatalog.identifyFromLabel("MTTL-W01 HU04139-17002C");
+        assertNotNull(p);
+        assertEquals("mttl_w01", p.id);
+        assertEquals(HardwareCatalog.Transport.MTTL_LOCAL_TCP, p.transport);
+    }
+
+    @Test public void unknownLabelDoesNotGuess() {
+        assertNull(HardwareCatalog.identifyFromLabel("generic power strip 1234"));
+    }
+}
