272b77d49fca4c52d32876d0630313553bdb45a5
NEXVARY Inc <325503741+nexvary@users.noreply.github.com>
2026-09-18 22:59:35 -0700

    Test away windows and runtime formatting

--- app/src/test/java/com/fgmachines/rck/LocalAutomationEngineTest.java
+++ app/src/test/java/com/fgmachines/rck/LocalAutomationEngineTest.java
@@ -6,6 +6,8 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import java.time.LocalTime;
+
 public class LocalAutomationEngineTest {
     @Test
     public void validatesTwentyFourHourTimes() {
@@ -28,5 +30,39 @@ public class LocalAutomationEngineTest {
     public void createsStableDeadlinePreferenceKey() {
         assertEquals("automation_deadline_AABBCCDDEEFF_3",
                 LocalAutomationEngine.deadlinePreferenceKey("AA:BB:CC:DD:EE:FF", 3));
+        assertEquals("automation_standby_deadline_AABBCCDDEEFF_2",
+                LocalAutomationEngine.standbyDeadlinePreferenceKey("AA:BB:CC:DD:EE:FF", 2));
+        assertEquals("automation_away_next_AABBCCDDEEFF",
+                LocalAutomationEngine.awayNextPreferenceKey("AA:BB:CC:DD:EE:FF"));
+    }
+
+    @Test
+    public void supportsNormalAndOvernightAwayWindows() {
+        assertTrue(LocalAutomationEngine.isWithinAwayWindow(
+                "18:00", "23:00", LocalTime.of(20, 0)));
+        assertFalse(LocalAutomationEngine.isWithinAwayWindow(
+                "18:00", "23:00", LocalTime.of(12, 0)));
+        assertTrue(LocalAutomationEngine.isWithinAwayWindow(
+                "22:00", "06:00", LocalTime.of(23, 30)));
+        assertTrue(LocalAutomationEngine.isWithinAwayWindow(
+                "22:00", "06:00", LocalTime.of(5, 30)));
+        assertFalse(LocalAutomationEngine.isWithinAwayWindow(
+                "22:00", "06:00", LocalTime.of(12, 0)));
+    }
+
+    @Test
+    public void keepsAwayIntervalsInsideConfiguredBounds() {
+        for (int i = 0; i < 50; i++) {
+            int value = LocalAutomationEngine.randomIntervalMinutes(10, 15);
+            assertTrue(value >= 10);
+            assertTrue(value <= 15);
+        }
+        assertEquals(7, LocalAutomationEngine.randomIntervalMinutes(7, 7));
+    }
+
+    @Test
+    public void formatsRuntimeDurations() {
+        assertEquals("0h 00m", OutletRuntimeStore.formatDuration(0));
+        assertEquals("2h 05m", OutletRuntimeStore.formatDuration(125L * 60L * 1000L));
     }
 }
